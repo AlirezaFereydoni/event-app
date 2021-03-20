491e7347c264fc4bcd7fb8848d67cb9239fa8708
Alireza <Alireza@arianalabs.com>
2021-03-20 12:04:21 +0330

    Styling to detail Components

--- components/event/event-detail/event-logistics.js
+++ components/event/event-detail/event-logistics.js
@@ -1,17 +1,17 @@
-import AddressIcon from '../icons/address-icon';
-import DateIcon from '../icons/date-icon';
-import LogisticsItem from './logistics-item';
-import classes from './event-logistics.module.css';
+import AddressIcon from "../../icons/address-icon";
+import DateIcon from "../../icons/date-icon";
+import LogisticsItem from "./logistics-item";
+import classes from "./event-logistics.module.css";
 
 function EventLogistics(props) {
   const { date, address, image, imageAlt } = props;
 
-  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   });
-  const addressText = address.replace(', ', '\n');
+  const addressText = address.replace(", ", "\n");
 
   return (
     <section className={classes.logistics}>
--- pages/events/[eventId]/index.js
+++ pages/events/[eventId]/index.js
@@ -1,8 +1,30 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummyData";
+import EventContent from "../../../components/event/event-detail/event-content";
+import EventLogistics from "../../../components/event/event-detail/event-logistics";
+import EventSummary from "../../../components/event/event-detail/event-summary";
+
 const SpecialEvent = () => {
-  return (
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const detailEvent = getEventById(eventId);
+
+  return detailEvent !== undefined ? (
     <div>
-      <h1>SpecialEvent</h1>
+      <EventSummary title={detailEvent.title} />
+
+      <EventLogistics
+        date={detailEvent.date}
+        address={detailEvent.location}
+        image={detailEvent.image}
+        imageAlt={detailEvent.title}
+      />
+      <EventContent>
+        <p>{detailEvent.description}</p>
+      </EventContent>
     </div>
+  ) : (
+    <p>Event Not Found</p>
   );
 };
 
