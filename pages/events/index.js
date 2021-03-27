00a17da291dafd6adce935a6c3367ba709d9fcb7
Alireza <Alireza@arianalabs.com>
2021-03-27 19:32:45 +0430

    Static Generation of Events Page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,8 @@
 import EventList from "../../components/event/list/list.jsx";
-import { getAllEvents } from "../dummyData";
+import { getAllEvents } from "../../controller";
 import SearchBox from "../../components/event/event-search";
 
-const EventPage = () => {
-  const eventItems = getAllEvents();
-
+const EventPage = ({ eventItems }) => {
   return (
     <div>
       <SearchBox />
@@ -12,5 +10,16 @@ const EventPage = () => {
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const allEvents = await getAllEvents();
+
+  return {
+    props: {
+      events: allEvents,
+    },
+    revalidate: 60,
+  };
+}
 
 export default EventPage;
