970647e87ba8b372f6c2221f3e0b524f354cf1ea
Alireza <Alireza@arianalabs.com>
2021-03-28 23:06:08 +0430

    Add Document File

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
 import Layout from "../components/layout/layout.jsx";
+import Head from "next/link";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Next m Events App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
