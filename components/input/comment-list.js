1ff9aaf336bdfd9249935cb7ba86bf5cd4f6e902
AlirezaFereydoni <AlirezaFereydoni@outlook.com>
2021-03-31 19:13:14 +0430

    Fetch Comments from Api and Show them

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -1,21 +1,18 @@
-import classes from './comment-list.module.css';
+import classes from "./comment-list.module.css";
 
-function CommentList() {
+function CommentList({ comments }) {
   return (
     <ul className={classes.comments}>
-      {/* Render list of comments - fetched from API */}
-      <li>
-        <p>My comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </li>
-      <li>
-        <p>My comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </li>
+      {comments.map(item => {
+        return (
+          <li key={item.id}>
+            <p>{item.comment}</p>
+            <div>
+              By <address>{item.name}</address>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
--- components/input/comments.js
+++ components/input/comments.js
@@ -1,29 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
-import CommentList from './comment-list';
-import NewComment from './new-comment';
-import classes from './comments.module.css';
+import CommentList from "./comment-list";
+import NewComment from "./new-comment";
+import classes from "./comments.module.css";
 
 function Comments(props) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState();
 
+  useEffect(() => {
+    if (showComments) {
+      fetch(`/api/comments/${eventId}`)
+        .then(res => res.json())
+        .then(res => setComments(res.comments));
+    }
+  }, [showComments]);
   function toggleCommentsHandler() {
-    setShowComments((prevStatus) => !prevStatus);
+    setShowComments(prevStatus => !prevStatus);
   }
 
   function addCommentHandler(commentData) {
     // send data to API
+    fetch(`/api/comments/${eventId}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(commentData),
+    })
+      .then(res => res.json())
+      .then(res => console.log(res));
   }
 
   return (
     <section className={classes.comments}>
-      <button onClick={toggleCommentsHandler}>
-        {showComments ? 'Hide' : 'Show'} Comments
-      </button>
+      <button onClick={toggleCommentsHandler}>{showComments ? "Hide" : "Show"} Comments</button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
+      {showComments && <CommentList comments={comments} />}
     </section>
   );
 }
