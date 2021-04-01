f1f64c976933bec64fff3e319303eac45a397427
AlirezaFereydoni <AlirezaFereydoni@outlook.com>
2021-04-01 17:04:17 +0430

    Error Handling For newsLetter Api

--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,22 +1,40 @@
 import { MongoClient } from "mongodb";
 
 const NewsletterRegistrationHandler = async (req, res) => {
+  const url =
+    "mongodb+srv://Alireza:9092654a@nextjseventcluster.zzufe.mongodb.net/newsletter?retryWrites=true&w=majority";
+
+  const connectToDatabase = async URL => {
+    return await MongoClient.connect(URL);
+  };
+
+  const insertingDocument = async (client, document) => {
+    const database = client.db();
+    const collection = database.collection("emails");
+    const result = await collection.insertOne(document);
+  };
+
   if (req.method === "POST") {
     if (!req.body.email || !req.body.email.includes("@")) {
       res.status(422).json({ message: "Invalid Email Entered" });
     }
     const newEmail = {
+      id: Math.random().toFixed(2) + new Date().toISOString(),
       email: req.body.email,
     };
+    let client;
+    try {
+      client = connectToDatabase(url);
+    } catch (err) {
+      res.status(500).json({ message: "Connecting to Database Failed!" });
+      return;
+    }
 
-    const url =
-      "mongodb+srv://Alireza:9092654a@nextjseventcluster.zzufe.mongodb.net/newsletter?retryWrites=true&w=majority";
-
-    const client = await MongoClient.connect(url);
-    const database = client.db();
-    const collection = database.collection("emails");
-    const result = await collection.insertOne(newEmail);
-    newEmail.id = result.insertedId;
+    try {
+      insertingDocument(client, newEmail);
+    } catch (err) {
+      res.status(500).json({ message: "Inserting Data Failed!" });
+    }
 
     res.status(200).json({ message: "Success", email: newEmail });
     client.close();
